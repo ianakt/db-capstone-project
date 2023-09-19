50f5b72b896c9ba32cb943b4f55419db693c8893
ianakt <68198233+ianakt@users.noreply.github.com>
2023-09-19 09:49:47 -0400

    Starting the Create Views from week 2

---
+++ SQLView_draft.sql
@@ -0,0 +1,7 @@
+CREATE VIEW OrdersView AS select OrderID, BillAmount, Quantity FROM Orders;
+
+SELECT column_name.tableAlias, column_name.tableAlias
+  FROM database 
+  INNER JOIN table_ AS tableAlias ON t1.name = t2.name;
+
+  
