e63a6e7992902dc1d44297890635dacfe24b7ce5
ianakt <68198233+ianakt@users.noreply.github.com>
2023-09-22 10:30:09 -0400

    Task are labelled, finished task 2 week 2

--- SQLView_draft.sql
+++ SQLView_draft.sql
@@ -1,40 +1,6 @@
-CREATE VIEW OrdersView AS select OrderID, BillAmount, Quantity FROM Orders;
 
-SELECT column_name.tableAlias, column_name.tableAlias
-  FROM database 
-  INNER JOIN table_ AS tableAlias ON t1.name = t2.name;
-
-  
--- Create an Orders Table with the following columns:
---   EmployeeID
-
--- Add employeeID to Orders
-
--- Add menu name to menus tables, what should menu id look like?
-
--- How to link Bookings and Orders Tables?
-
--- SELECT * 
--- FROM Bookings 
---   JOIN Orders ON Orders.TableNo = Bookings.TableNo
-
-
-
--- SELECT *
--- FROM menus
---   JOIN menuitems ON menuitems.ItemID = menus.ItemID
-
--- SELECT * 
--- FROM Orders 
---   JOIN Menus ON Orders.MenuID = menus.MenuID
---   JOIN menuitems ON menuitems.ItemID = menus.ItemID
---   JOIN Bookings ON Orders.TableNo = Bookings.TableNo
-
--- SELECT Orders.OrderID, Orders.Quantity, Orders.BillAmount, menuitems.Price
--- FROM Orders 
---   JOIN Menus ON Orders.MenuID = menus.MenuID
---   JOIN menuitems ON menuitems.ItemID = menus.ItemID
---   JOIN Bookings ON Orders.TableNo = Bookings.TableNo
+--Task 1 week 2 Created Orders view
+--CREATE VIEW OrdersView AS select OrderID, BillAmount, Quantity FROM Orders;
 
 
 CREATE VIEW OrdersView AS 
@@ -45,3 +11,18 @@ SELECT DISTINCT Orders.OrderID, Orders.Quantity, Orders.BillAmount
   JOIN Bookings ON Orders.TableNo = Bookings.TableNo
   WHERE 
     Orders.Quantity > 2
+
+-- Task 2 Week 2  
+SELECT DISTINCT Orders.OrderID, Orders.BillAmount, employees.Name, employees.EmployeeID, menus.Cuisine, menuitems.Name
+  FROM Orders 
+  JOIN Menus ON Orders.MenuID = menus.MenuID
+  JOIN menuitems ON menuitems.ItemID = menus.ItemID
+  JOIN Bookings ON Orders.TableNo = Bookings.TableNo
+  JOIN employees on employees.EmployeeID = Bookings.EmployeeID
+
+
+-- Task 3 Week 3
+Select * 
+  FROM Orders 
+  JOIN Menus ON Orders.MenuID = menus.MenuID
+  JOIN menuitems ON menuitems.ItemID = menus.ItemID
